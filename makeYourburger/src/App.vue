de98ce483998e655da87e5a362af0f1cb62cb41c
marcelogam <106875860+marcelogam@users.noreply.github.com>
2024-01-28 21:59:23 -0300

    Projeto finalizado

---
+++ makeYourburger/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <Navbar :logo="logoSrc" :alt="appName" />
+  <router-view></router-view>
+  <Footer />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logoSrc: "/img/logo.png",
+      appName: "Make your Burger",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-countainer {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
---
+++ makeYourburger/src/views/Pedidos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-countainer">
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from "../components/Dashboard.vue";
+export default {
+  components: {
+    Dashboard,
+  },
+  name: "Pedido",
+};
+</script>
+
+<style scoped>
+</style>>
+
